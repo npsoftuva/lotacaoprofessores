e85f7b1abdd8e910b913a8365ba048942e1b1626
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2017-05-05 16:13:55 -0300

    Acoes View Lotacao

--- view/lotacao.php
+++ view/lotacao.php
@@ -105,7 +105,7 @@
                           <td>
                             <a data-toggle="modal" data-cod="1" title="Editar" class="openEdit btn btn-warning" href="#edit"><span class="pe-7s-note" aria-hidden="true"></span></a>
 
-                            <a data-toggle="modal" data-cod="1" title="Excluir" class="openDelete btn btn-danger" href="#delete"><span class="pe-7s-trash" aria-hidden="true"></span></a>
+                            <a data-toggle="modal" data-cod="1" title="Excluir" data-cod="222" class="openDelete btn btn-danger" href="#delete"><span class="pe-7s-trash" aria-hidden="true"></span></a>
                           </td>
                         </tr>
 <?php } ?>
@@ -131,7 +131,7 @@
           </div>
           <div class="modal-footer">
             <form role="form" method="POST">
-              <input type="hidden" name="ofr_codx" id="ofr_codx" value="">
+              <input type="hidden" name="lot_codx" id="lot_codx" value="">
               <input type="button" class="btn btn-danger btn-fill" data-dismiss="modal" value="Não">
               <input type="submit" class="btn btn-success btn-fill" value="Sim" name="Excluir">
             </form>
@@ -150,44 +150,49 @@
           </div>
           <form role="form" method="POST">
             <div class="modal-body">
-              <input type="hidden" name="ofr_cod" id="ofr_cod" value="">
-              <div class="form-group">
-                <label>Período *</label>
-                <select class="form-control" name="prd_cod" id="prd_cod">
-                  <option value="<?php echo $lastPeriodo->__get("prd_cod"); ?>"><?php echo $lastPeriodo->__get("prd_cod"); ?></option>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Semestre *</label>
-                <select class="form-control" name="cmp_sem" id="cmp_sem">
-                  <?php
-                  for ($i = 1; $i <= $lastFluxo->__get("flx_sem"); $i++) { ?>
-                  <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Disciplina *</label>
-                <select class="form-control" name="dcp_cod" id="dcp_cod">
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Fluxo *</label>
-                <select class="form-control" name="flx_cod" id="flx_cod">
-                  <?php foreach ($fluxos as $fluxo) { ?>
-                  <option value="<?php echo $fluxo->__get("flx_cod"); ?>"><?php echo $fluxo->__get("flx_cod"); ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Turma *</label>
-                <input class="form-control" type="number" placeholder="Nº da Turma" id="ofr_trm" name="ofr_trm" min="1" required autocomplete="off">
-              </div>
-              <div class="form-group">
-                <label>Vagas *</label>
-                <input class="form-control" type="number" placeholder="Quat. de vagas da lotacao" id="ofr_vag" name="ofr_vag" min="1" required autocomplete="off">
+            <div class="form-group">
+              <label>Oferta *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Professor *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Sala *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <div class="content table-responsive table-full-width">
+                <table class="table table-striped table-hover" id="dataTables-example">
+                  <thead>
+                    <th> </th>
+                    <?php for ($i = ord('A'); $i <= ord('Q'); $i++) { ?>
+                      <th><?php echo chr($i); ?></th>
+                    <?php } ?>
+                  </thead>
+                  <tbody>
+                    <?php for ($i = 2; $i <= 7; $i++) { ?>
+                    <tr>
+                      <td><?php echo $i; ?></td>
+                      <?php for ($c = ord('A'); $c <= ord('Q'); $c++) { ?>
+                      <td>
+                        <input type="checkbox">
+                      </td>
+                      <?php } ?>
+                    </tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
               </div>
             </div>
+          </div>
             <div class="modal-footer">
               <input type="button" class="btn btn-warning btn-fill" data-dismiss="modal" value="Cancelar">
               <input type="submit" class="btn btn-success btn-fill" value="Salvar" name="Editar">
@@ -203,51 +208,57 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="add"><span class="pe-7s-users"></span> Adicionar Lotacao</h4>
+            <h4 class="modal-title" id="add"><span class="pe-7s-notebook"></span> Adicionar Lotação</h4>
           </div>
           <form role="form" method="POST">
-            <div class="modal-body">
-              <div class="form-group">
-                <label>Período *</label>
-                <select class="form-control" name="prd_coda" id="prd_coda">
-                  <option value="<?php echo $lastPeriodo->__get("prd_cod"); ?>"><?php echo $lastPeriodo->__get("prd_cod"); ?></option>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Semestre *</label>
-                <select class="form-control" name="cmp_sema" id="cmp_sema">
-                  <?php
-                  for ($i = 1; $i <= $lastFluxo->__get("flx_sem"); $i++) { ?>
-                  <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Disciplina *</label>
-                <select class="form-control" name="dcp_coda" id="dcp_coda">
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Fluxo *</label>
-                <select class="form-control" name="flx_coda" id="flx_coda">
-                  <?php foreach ($fluxos as $fluxo) { ?>
-                  <option value="<?php echo $fluxo->__get("flx_cod"); ?>"><?php echo $fluxo->__get("flx_cod"); ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Turma *</label>
-                <input class="form-control" type="number" placeholder="Nº da Turma" id="ofr_trma" name="ofr_trma" min="1" required autocomplete="off">
-              </div>
-              <div class="form-group">
-                <label>Vagas *</label>
-                <input class="form-control" type="number" placeholder="Quat. de vagas da lotacao" id="ofr_vaga" name="ofr_vaga" min="1" required autocomplete="off">
+          <div class="modal-body">
+            <div class="form-group">
+              <label>Oferta *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Professor *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Sala *</label>
+              <select class="form-control" name="flx_cod" id="flx_cod">
+                <option value="0">0</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <div class="content table-responsive table-full-width">
+                <table class="table table-striped table-hover" id="dataTables-example">
+                  <thead>
+                    <th> </th>
+                    <?php for ($i = ord('A'); $i <= ord('Q'); $i++) { ?>
+                      <th><?php echo chr($i); ?></th>
+                    <?php } ?>
+                  </thead>
+                  <tbody>
+                    <?php for ($i = 2; $i <= 7; $i++) { ?>
+                    <tr>
+                      <td><?php echo $i; ?></td>
+                      <?php for ($c = ord('A'); $c <= ord('Q'); $c++) { ?>
+                      <td>
+                        <input type="checkbox">
+                      </td>
+                      <?php } ?>
+                    </tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
               </div>
             </div>
-              <div class="modal-footer">
-                <input type="submit" class="btn btn-success btn-fill" value="Adicionar" name="Adicionar" id="Adicionar">
-                <button type="reset" class="btn btn-warning btn-fill">Limpar</button>
-              </div>
+          </div>
+          <div class="modal-footer">
+            <input type="submit" class="btn btn-success btn-fill" value="Adicionar" name="Adicionar" id="Adicionar">
+            <button type="reset" class="btn btn-warning btn-fill">Limpar</button>
+          </div>
           </form>
         </div>
       </div>
@@ -303,8 +314,8 @@
 
   <script type="text/javascript">
     $(document).on("click", ".openDelete", function () {
-      var ofr_cod = $(this).data('cod');
-      $(".modal-footer #ofr_codx").val(ofr_cod);
+      var lot_cod = $(this).data('cod');
+      $(".modal-footer #lot_codx").val(lot_cod);
     });
   </script>
   <script>
